8ca06d423429927c044bc1891c01a223ef22a131
maniollo <mariusz.maslanka@gmail.com>
2020-10-30 13:14:41 +0100

    I command you

--- src/main/java/masmar/home/jba/encryptiondecryption/Main.java
+++ src/main/java/masmar/home/jba/encryptiondecryption/Main.java
@@ -1,14 +1,37 @@
 package masmar.home.jba.encryptiondecryption;
 
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
         EncryptionService encryptionService = new EncryptionService();
-        Scanner scanner = new Scanner(System.in);
-        String text = scanner.nextLine();
-        int key = scanner.nextInt();
-        String encrypted = encryptionService.keyEncryption(text, key);
-        System.out.println(encrypted);
+        String mode = "enc";
+        int key = 0;
+        String data = "";
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("-mode")) {
+                mode = args[i++ + 1];
+                continue;
+            }
+
+            if (args[i].equals("-key")) {
+                key = Integer.parseInt(args[i++ + 1]);
+                continue;
+            }
+
+            if (args[i].equals("-data")) {
+                data = args[i++ + 1];
+            }
+        }
+
+        String result = "";
+        switch (mode) {
+            case "enc":
+                result = encryptionService.keyEncryption(data, key);
+                break;
+            case "dec":
+                result = encryptionService.keyDecryption(data, key);
+                break;
+        }
+
+        System.out.println(result);
     }
 }
