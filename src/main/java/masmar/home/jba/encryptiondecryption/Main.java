1981c90f64f41a07f794d4dbe329f81f1250a037
maniollo <mariusz.maslanka@gmail.com>
2020-10-30 17:00:42 +0100

    X-Files

--- src/main/java/masmar/home/jba/encryptiondecryption/Main.java
+++ src/main/java/masmar/home/jba/encryptiondecryption/Main.java
@@ -1,11 +1,21 @@
 package masmar.home.jba.encryptiondecryption;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 class Main {
     public static void main(String[] args) {
         EncryptionService encryptionService = new EncryptionService();
         String mode = "enc";
         int key = 0;
         String data = "";
+        String in = null;
+        String out = null;
+
         for (int i = 0; i < args.length; i++) {
             if (args[i].equals("-mode")) {
                 mode = args[i++ + 1];
@@ -17,9 +27,27 @@ class Main {
                 continue;
             }
 
+            if (args[i].equals("-in")) {
+                try {
+                    in = Files.readAllLines(Paths.get(args[i++ + 1])).get(0);
+                    continue;
+                } catch (IOException e) {
+                    System.out.println("File does not exist!");
+                }
+            }
+
+            if (args[i].equals("-out")) {
+                out = args[i++ + 1];
+                continue;
+            }
+
             if (args[i].equals("-data")) {
                 data = args[i++ + 1];
             }
+        }
+
+        if (data.isEmpty() && Objects.nonNull(in)) {
+            data = in;
         }
 
         String result = "";
@@ -32,6 +60,18 @@ class Main {
                 break;
         }
 
-        System.out.println(result);
+        if (Objects.isNull(out)) {
+            System.out.println(result);
+        } else {
+            saveInto(out, result.getBytes());
+        }
+    }
+
+    private static void saveInto(String outputFileName, byte[] encoded) {
+        try (OutputStream outputStream = new FileOutputStream(outputFileName, false)) {
+            outputStream.write(encoded);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
